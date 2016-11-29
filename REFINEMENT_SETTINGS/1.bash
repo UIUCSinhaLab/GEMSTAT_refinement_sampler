cb02a5297d8c6901b188003a85af34eaccd087c9
Bryan Lunt <bjlunt2@illinois.edu>
2016-11-29 13:55:08 -0600

    renamed and moved files

--- REFINEMENT_SETTINGS/1.bash
+++ REFINEMENT_SETTINGS/1.bash
@@ -1,6 +1,6 @@
 
 
-method_names="EXAMPLE.bash new.bash"
+method_names="EXAMPLE new"
 
 TRAIN_ORTHO=mel
 
@@ -8,6 +8,10 @@ N_TO_REFINE=1
 ENSEMBLE_NAME="example_ensemble"
 
 DATA_ORIGIN="data_hassan_cic_baked_zld_free"
+
+
+export NA_CYCLES=2
+
 
 method_1_extra_settings="NA_CYCLES=5"
 method_2_extra_settings="NA_CYCLES=2 COPIES=20 SIGMA0=0.05 SIGMA1=0.0"
