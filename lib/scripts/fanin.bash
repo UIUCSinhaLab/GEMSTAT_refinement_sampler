e609601e12b1ee707cb53c31404e33a3a33da309
Bryan Lunt <bjlunt2@illinois.edu>
2018-03-31 15:48:57 -0500

    improvement to fanin system

--- lib/scripts/fanin.bash
+++ lib/scripts/fanin.bash
@@ -24,11 +24,25 @@ export > ${JOBBASE}/final.bash
 #assume BASE exists
 
 method_sample_dir=${JOBBASE}/samples/method_${method_name}/
+zip_archive_dir=${method_sample_dir}/zips/
+mkdir -p ${zip_archive_dir}
+
 
 #this can be changed later if we decide we want to stage the data in
 datadir_to_use=${JOBBASE}/data
 
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/shared-mounts/sinhas-storage1/INFRASTRUCTURE/HAL/JUPYTER/python/lib/
-PYTHONPATH=${PYTHONPATH}:${BASE}/lib/GEMSTAT_scripts/python/src/
+PYTHONPATH=${PYTHONPATH}:${BASE}/lib/GEMSTAT_scripts/python/src/:${BASE}/lib/notebook_core/:${BASE}/lib/sampling_core/:
 
+
+#Run the fanin script
 PYTHONPATH=${PYTHONPATH} python ${BASE}/lib/python/sampling_core/fanin.py ${JOBBASE}/crossval/${method_name}.hd5 ${method_sample_dir} $(seq ${START_N} ${END_N})
+
+
+#zip up all the files that were used.
+for i in $( seq ${START_N} ${END_N} )
+do
+
+	zip -g ${zip_archive_dir}/batch_${START_N}_${END_N}.zip -m ${method_sample_dir}/out/${i}.out -m ${method_sample_dir}/out/${i}.par -m ${method_sample_dir}/log/${i}.log -m ${method_sample_dir}/log/*_${i}.log -m ${method_sample_dir}/crossval/*_${i}.out
+
+done
